14f57336c670c3f69661cfed9d75d6c9170c21f3
APRENDIZ <APRENDIZ@BOGDCDM093>
2024-08-01 14:12:50 -0500

    se crea funcionalidad del cajero

---
+++ cajero/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al Cajero Automático");
+            // Aquí se agregará la lógica del cajero automático
+        }
+    }
+}
